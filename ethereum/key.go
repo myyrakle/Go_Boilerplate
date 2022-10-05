14f72cd22fe3fd745ee12eab18f3c4389b262b4b
myyrakle <sssang97@naver.com>
2022-10-05 18:04:27 +0900

    Update key.go

--- ethereum/key.go
+++ ethereum/key.go
@@ -60,3 +60,42 @@ func MakeAddress() (string, error) {
 
 	return address, nil
 }
+
+func createKeystore() {
+	ks := keystore.NewKeyStore("./tmp", keystore.StandardScryptN, keystore.StandardScryptP)
+	password := "..."
+	account, err := ks.NewAccount(password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(account.Address.Hex()) // 지갑 주소
+}
+
+func importKeystore() {
+	ks := keystore.NewKeyStore("./tmp2", keystore.StandardScryptN, keystore.StandardScryptP)
+
+	filePath := "./tmp/UTC--2022-10-05T06-19-14.753221975Z--75a1d322d5c5bd1b98ae8061517f036e750f4f7c"
+
+	jsonBytes, err := ioutil.ReadFile(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	password := "..."
+
+	// account 가져오기
+	account, err := ks.Import(jsonBytes, password, password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(account.Address.Hex())
+
+	// 기존 파일 삭제
+	if err := os.Remove(filePath); err != nil {
+		log.Fatal(err)
+	}
+}
